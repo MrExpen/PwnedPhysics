1cbcf0168732616771a3887b3b8166ebdf3d398c
Dmitrii Chibrikov <chi-dimochka@yandex.ru>
2023-01-15 12:40:57 +0400

    Initial commit

---
+++ PwnedPhysics/Controllers/VerifyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PwnedPhysics.Controllers;
+
+[ApiController]
+public class VerifyController : ControllerBase
+{
+    [Route("verify")]
+    public string Verify([FromQuery] string? code)
+    {
+        return $"{code}verified";
+    }
+}
